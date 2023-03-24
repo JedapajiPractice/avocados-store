07cea8d09b9f75651b4e7a41427d418d0383d376
jedapaji <jessicadpaez@outlook.com>
2023-03-24 18:57:14 -0500

    Ajuste index

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   mount: {
     public: '/',
-    src: '/_dist_',
+    src: '/_dist/index.css',
   },
   buildOptions: {
     baseUrl: 'https://JedapajiPractice.github.io/Avocados-Store',
